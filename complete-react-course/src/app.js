dee5b45e1f0ffdfb4f3246a700c3a5cea14c76fa
manuraj23 <manuraj082004@gmail.com>
2025-01-22 23:52:57 +0530

    Learned Using Bootstrap

--- complete-react-course/src/app.js
+++ complete-react-course/src/app.js
@@ -5,12 +5,19 @@ function App(){
     // const p=document.createElement('p');
     // p.textContent='This is demo Paragraph';
     // // return <h2>This is app component</h2>
-    let msg='awesome';
+    // let msg='awesome';
     // return <div>
     //     <h1 className="myHeader">Hello</h1>
     //     <p className="myParagraph">This is my first component {10 + 5} {msg}</p>
     //     </div>;
-    return <Products></Products>
-}
+    return (
+        <div className='row'>
+            <div className='col-md-8 mx-auto'>
+                <Products></Products>
+            </div>
+        </div>
+    );
+    
+} 
 
 export default App;
--- complete-react-course/src/componets/product.js
+++ complete-react-course/src/componets/product.js
@@ -1,18 +1,28 @@
-import React from 'react'; 
+let productCount=10;
+
+function displayFormattedProductCount(){
+    return productCount>0?productCount:'Zero';
+    // return productCount > 0 ? productCount : <h1>Zero</h1>;
+};
 
 function Products(){
-    return (<ul>
-        <li>
-            <div>
-                <div>
-                    <h5>Fresh Milk</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
-                    <div>
-                        <h6>$120.00</h6>
-                    </div>
-                </div><img src="" width="200" />
-            </div>
-        </li>
-    </ul>);
+    return (
+        <ul className="list-group shadow">
+            <li className="list-group-item">
+                <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
+                    <div className="media-body order-2 order-lg-1">
+                        <h5 className="mt-0 font-weight-bold mb-2">Fresh Milk</h5>
+                        <p className="font-italic text-muted mb-0 small">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit fuga autem maiores necessitatibus.</p>
+                        <div className="d-flex align-items-center justify-content-start mt-1">
+                            <h6 className="font-weight-bold my-2">$120.00</h6>
+                            <button className="btn btn-primary">-</button>
+                            <span>{displayFormattedProductCount()}</span>
+                            <button className="btn btn-primary">+</button>
+                        </div>
+                    </div><img src="" alt="Generic placeholder image" width="200" class="ml-lg-5 order-1 order-lg-2" />
+                </div>
+            </li>
+        </ul>
+    );
 }
 export default Products;
